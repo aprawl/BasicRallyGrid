50b648c5380caf808adb91ffb396c81139c4f6ec
aprawl <andrew.prawl@gmail.com>
2014-06-18 10:57:13 -0400

    Adding the Rally grid files into the repo

---
+++ .bash_history
@@ -0,0 +1,3 @@
+cd dev/
+git clone git@github.com:aprawl/BasicRallyGrid.git
+rally-app-builder init BasicRallyGrid
---
+++ App.js
@@ -0,0 +1,85 @@
+Ext.define('CustomApp', {
+    extend: 'Rally.app.App',
+    componentCls: 'app',
+    items:{ html:'<a href="https://help.rallydev.com/apps/2.0rc3/doc/">App SDK 2.0rc3 Docs</a>'},
+    
+    launch: function() {
+
+        console.log('Our First App');
+
+        this._loadData();
+    },
+
+    _loadData: function() {
+
+    var myStore = Ext.create('Rally.data.wsapi.Store', {
+    model: 'User Story',
+    autoLoad: true,
+    listeners: {
+        load: function(myStore, myData, success) {
+            //process data
+            console.log('got data!', myStore, myData, success);
+this._loadGrid(myStore);
+            // var myGrid = Ext.create('Rally.ui.grid.Grid', {
+            // 	store: myStore,
+            // 	columnCfgs: [
+            // 	'FormattedID', 'Name', 'Owner', 'ScheduleState'
+            // 	]
+            // });
+
+            // console.log('my grid', myGrid);
+
+            // this.add(myGrid);
+            // console.log('what is this?', this);
+        },
+        scope: this
+    },
+    fetch: ['FormattedID', 'Name', 'Owner', 'ScheduleState']
+});
+    
+    },
+
+    _loadGrid: function(myStoryStore) {
+var myGrid = Ext.create('Rally.ui.grid.Grid', {
+            	store: myStoryStore,
+            	columnCfgs: [
+            	'FormattedID', 'Name', 'Owner', 'ScheduleState'
+            	]
+            });
+
+            console.log('my grid', myGrid);
+
+            this.add(myGrid);
+            console.log('what is this?', this);
+    }
+
+// launch: function() {
+        //Write app code here
+        // console.log('Our First App');
+
+//         var myStore = Ext.create('Rally.data.wsapi.Store', {
+//     model: 'User Story',
+//     autoLoad: true,
+//     listeners: {
+//         load: function(myStore, myData, success) {
+//             //process data
+//             console.log('got data!', myStore, myData, success);
+
+//             var myGrid = Ext.create('Rally.ui.grid.Grid', {
+//             	store: myStore,
+//             	columnCfgs: [
+//             	'FormattedID', 'Name', 'Owner', 'ScheduleState'
+//             	]
+//             });
+
+//             console.log('my grid', myGrid);
+
+//             this.add(myGrid);
+//             console.log('what is this?', this);
+//         },
+//         scope: this
+//     },
+//     fetch: ['FormattedID', 'Name', 'Owner', 'ScheduleState']
+// });
+//     }
+});
