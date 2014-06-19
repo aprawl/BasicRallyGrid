65fff88b37ef2b61579d689447348b1a59fc8028
aprawl <andrew.prawl@gmail.com>
2014-06-19 14:59:55 -0400

    Added a drop down box.  The content of the drop drown box still needs to be added to account for tags.

--- App.js
+++ App.js
@@ -49,6 +49,24 @@ app._loadGrid(myStore);
 
     _loadGrid: function(myStoryStore) {
         // var app = this;
+var dropDown = Ext.create('Ext.button.Split', {
+    renderTo: Ext.getBody(),
+    text: 'Options',
+    // handle a click on the button itself
+    handler: function() {
+        alert("The button was clicked");
+    },
+    menu: new Ext.menu.Menu({
+        items: [
+            // these will render as dropdown menu items when the arrow is clicked:
+            {text: 'Item 1', handler: function(){ alert("Item 1 clicked"); }},
+            {text: 'Item 2', handler: function(){ alert("Item 2 clicked"); }}
+        ]
+    })
+});
+            app.add(dropDown);
+            console.log('what is this?', this);
+
 var myGrid = Ext.create('Rally.ui.grid.Grid', {
             	store: myStoryStore,
             	columnCfgs: [
